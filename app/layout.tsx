e75b2feb4a82af7f204441b5f2d6cc7b13dd051d
Jim <jim.alpez06@gmail.com>
2024-01-13 01:42:43 +0800

    fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <div className="bg-sky-100 -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-rose-100 -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
-        <Wrapper children={children} />
+        <Wrapper>{children}</Wrapper>
       </body>
     </html>
   );
