710f265aabb901b53d2b1dca97a9e033d6bd1a9e
Jim <jim.alpez06@gmail.com>
2024-01-16 01:11:59 +0800

    replace the .. to @

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import About from "../components/about";
-import Contact from "../components/contact";
-import Intro from "../components/intro";
-import Portfolio from "../components/portfolio";
-import Skills from "../components/skills";
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Intro from "@/components/intro";
+import Portfolio from "@/components/portfolio";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
